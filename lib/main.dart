dfeda3ab1532688d676fd6e1b90a3d99747c70b2
Tao Dong <taodong@google.com>
2019-09-23 17:02:54 -0700

    Add a second RenderFlex Overflow error

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_error_studies/missing_material_error.dart';
 import 'package:flutter_error_studies/renderflex_overflow_error.dart';
+import 'package:flutter_error_studies/renderflex_overflow_error2.dart';
 import 'package:flutter_error_studies/scaffold_context_error.dart';
 import 'package:flutter_error_studies/thrown_exception_error.dart';
 import 'package:flutter_error_studies/unbounded_viewport_error.dart';
@@ -52,6 +53,11 @@ class _MyHomePageState extends State<MyHomePage> {
                 context,
                 "RenderFlex Overflow",
                 TextClippingError(),
+              ),
+              makeDemoEntry(
+                context,
+                "RenderFlex Overflow 2",
+                ColumnOverflow(),
               ),
               makeDemoEntry(
                 context,
---
+++ lib/renderflex_overflow_error2.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class ColumnOverflow extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('RenderFlex OverFlow 2'),
+        ),
+        body: Container(
+          child: Row(
+            children: [
+              Icon(Icons.message),
+              // Resolution: Wrap the Column below in an Expanded widget.
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Title", style: Theme.of(context).textTheme.display1),
+                  Text(
+                      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed"
+                      " do eiusmod tempor incididunt ut labore et dolore magna "
+                      "aliqua. Ut enim ad minim veniam, quis nostrud "
+                      "exercitation ullamco laboris nisi ut aliquip ex ea "
+                      "commodo consequat."),
+                ],
+              ),
+            ],
+          ),
+          decoration: new BoxDecoration(
+            color: Colors.grey[300],
+          ),
+          width: 400.0,
+        ),
+      ),
+    );
+  }
+}
